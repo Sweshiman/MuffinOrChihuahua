a39d8b49f4e8c3d7f4e826bb89e40163f7c4779e
Alexander Karlsson <alex94@comhem.se>
2018-10-09 15:13:57 +0200

    Added an animation, fancy shit needs to be added to handle parallel animations

--- src/arduino/main/main.ino
+++ src/arduino/main/main.ino
@@ -1,15 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(38, 2);
+#define NumOfStrips 1
+
 String buffer = "";
 int pot0 = 0;
+Adafruit_NeoPixel strips[] = {Adafruit_NeoPixel(39, 2)};
 
 void setup() {
   Serial.begin(9600);
   pinMode(A0, OUTPUT);//POT0
   pot0 = analogRead(A0);
-  strip.begin();
-  strip.show();
+  for(int i = 0; i<NumOfStrips; i++){
+    strips[i].begin();
+    strips[i].show();
+  }
 }
 
 void loop() {
@@ -24,13 +28,23 @@ void loop() {
   }
 
   //SEND NEW VALUES IF THEY ARE NEW
-  newPot0 = analogRead(A0);
+  int newPot0 = analogRead(A0);
   if(abs(pot0 - newPot0) < 20){
     Serial.print("P:0:");
     Serial.println(newPot0);
   }
+  pulseOne(0,0,0,5);
+  pulseOne(0,0,0,0);
 }
 
 void handleCommand(String cmd){
   //Handle the command
+}
+
+void pulseOne(int stripId, int r, int g, int b){
+  for(int i = 0; i<strips[stripId].numPixels(); i++){
+    strips[stripId].setPixelColor(i,r,g,b);
+    strips[stripId].show();
+    delay(25);
+  }
 }
