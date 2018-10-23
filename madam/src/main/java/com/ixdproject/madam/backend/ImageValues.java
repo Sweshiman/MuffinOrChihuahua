11bfaec82fd576ae86c38f22df924dcd1682673f
MattiasJo <mattiasj81@gmail.com>
2018-10-23 10:57:58 +0200

    Merge branch 'master' of https://github.com/Sweshiman/MuffinOrChihuahua

--- madam/src/main/java/com/ixdproject/madam/backend/ImageValues.java
+++ madam/src/main/java/com/ixdproject/madam/backend/ImageValues.java
@@ -3,20 +3,18 @@ package com.ixdproject.madam.backend;
 import java.util.HashMap;
 
 public class ImageValues {
-    // 0 - 1024
+    // 0 - 10
     private HashMap<ImageManager.Img, MufChiValues> imageValues = new HashMap<>();
 
     public ImageValues() {
-        imageValues.put(ImageManager.Img.CHIHUAHUA1, new MufChiValues(23, 42, 12));
-        imageValues.put(ImageManager.Img.CHIHUAHUA2, new MufChiValues(432, 114, 42));
-        imageValues.put(ImageManager.Img.CHIHUAHUA3, new MufChiValues(643, 34, 12));
-        imageValues.put(ImageManager.Img.CHIHUAHUA4, new MufChiValues(91, 426, 512));
-        imageValues.put(ImageManager.Img.CHIHUAHUA5, new MufChiValues(232, 132, 102));
-        imageValues.put(ImageManager.Img.MUFFIN1, new MufChiValues(21, 12, 12));
-        imageValues.put(ImageManager.Img.MUFFIN2, new MufChiValues(21, 12, 12));
-        imageValues.put(ImageManager.Img.MUFFIN3, new MufChiValues(21, 12, 12));
-        imageValues.put(ImageManager.Img.MUFFIN4, new MufChiValues(21, 12, 12));
-        imageValues.put(ImageManager.Img.MUFFIN5, new MufChiValues(21, 12, 12));
+        imageValues.put(ImageManager.Img.CHIHUAHUA1, new MufChiValues(3, 8, 9));
+        imageValues.put(ImageManager.Img.CHIHUAHUA2, new MufChiValues(8, 1, 9));
+        imageValues.put(ImageManager.Img.CHIHUAHUA3, new MufChiValues(8, 7, 9));
+        imageValues.put(ImageManager.Img.CHIHUAHUA4, new MufChiValues(9, 4, 3));
+        imageValues.put(ImageManager.Img.MUFFIN1, new MufChiValues(2, 1, 9));
+        imageValues.put(ImageManager.Img.MUFFIN2, new MufChiValues(3, 9, 2));
+        imageValues.put(ImageManager.Img.MUFFIN3, new MufChiValues(5, 2, 3));
+        imageValues.put(ImageManager.Img.MUFFIN4, new MufChiValues(5, 3, 4));
     }
 
     public MufChiValues getImageValues(ImageManager.Img img) {
